8949c36da07acd02e02074fb8f3586ee4681e664
dwitcher <31394905+dwitcher@users.noreply.github.com>
2017-11-08 13:21:00 -0600

    Added logic for add/remove employee - 11.8.2017

--- Admin.java
+++ Admin.java
@@ -5,7 +5,7 @@ public class Admin extends Employee
 
     public Admin()
     {
-
+        
     }
 
     public void adjustPayroll()
@@ -18,14 +18,26 @@ public class Admin extends Employee
 
     }
 
-    public void addEmployee()
+    public void addEmployee(Employee emp)
     {
-
+        //Add the passed in employee object into the current data structure for storing employee information
     }
 
-    public void removeEmployee()
+    public void removeEmployee(int employeeID)
     {
-
+        //Search the employee information data structure and remove the item which has the specified employee ID
+        
+        /*
+        if (employee does exist in list or is found)
+        {
+            //Remove from list
+            System.out.println("Employee removed from list.");
+        }
+        else
+        {
+            System.out.println("Specified employee ID does not exist.");
+        }
+        */
     }
 
 }
